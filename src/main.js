7399a056c8917a8bed41a70691cafb376d68bd14
irfan1991 <irfanprasetyo91@gmail.com>
2021-03-29 14:08:38 +0700

    uplod projecT

--- src/main.js
+++ src/main.js
@@ -1,4 +1,40 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/**
+ * import Toastr
+ */
+
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+/**
+ * Tailwind CSS
+*/
+import './index.css';
+/**
+ * Mixins
+ */
+import mixins from './mixins'
+
+/**
+ * Vue Router
+ */
+import router from './router'
+/**
+ * Vuex  
+ */
+import store from './store'
+
+
+const app = createApp(App)
+
+app.use(Toast)
+
+app.mixin(mixins)
+
+app.use(router)
+
+app.use(store)
+
+app.mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,38 @@
+//import vuex
+import { createStore } from 'vuex'
+
+
+//import module auth
+import auth from './module/auth'
+
+//import module donation
+import donation from './module/donation'
+
+//import module profile
+import profile from './module/profile'
+
+
+//import module slider
+import slider from './module/slider'
+
+//import module category
+import category from './module/category'
+
+//import module campaign
+import campaign from './module/campaign'
+
+
+
+//create store vuex
+export default createStore({
+
+    modules: {
+        auth,
+        donation,
+        profile,
+        slider,
+        category,
+        campaign,
+    }
+
+})
